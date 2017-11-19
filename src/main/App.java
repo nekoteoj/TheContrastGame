62c8bac44eb4fbe2b0f22d5a6aced548106ca784
Pisit Wajanasara <compisit1234@gmail.com>
2017-11-19 20:18:43 +0700

    Edit main App

--- src/main/App.java
+++ src/main/App.java
@@ -1,14 +1,36 @@
 package main;
 
+import controller.MainController;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import view.ViewManager;
 
 public class App extends Application {
+	
+	public static int SCREEN_WIDTH = 800;
+	public static int SCREEN_HEIGHT = 600;
+	
+	private ViewManager viewManager;
+	private MainController mainController;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		viewManager = new ViewManager(primaryStage);
+		
+		mainController = new MainController();
+		mainController.on();
+		
 		primaryStage.setTitle("The Contrast Game");
 		primaryStage.show();
+		
+		primaryStage.setOnCloseRequest(event -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		
+		viewManager.goTo("menu");
 	}
 
 	public static void main(String[] args) {
