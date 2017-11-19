31e533d4cb0a9876b27e048ae5ac899662bbe62c
Pisit Wajanasara <compisit1234@gmail.com>
2017-11-19 20:14:37 +0700

    Add Controller

---
+++ src/controller/GameCanvasController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import javafx.scene.input.KeyCode;
+import logic.GameMap;
+import model.Hero;
+import model.Movable;
+import view.GameCanvas;
+
+public class GameCanvasController {
+	
+	GameCanvas gameCanvas;
+	
+	public GameCanvasController(GameCanvas gameCanvas) {
+		this.gameCanvas = gameCanvas;
+	}
+	
+	public void listenEvent() {
+		gameCanvas.setOnKeyPressed(event -> {
+			KeyCode code = event.getCode();
+			if (code.isArrowKey()) {
+				if (code == KeyCode.LEFT) {
+					GameMap.getRenderObjects().parallelStream()
+						.filter(x -> x instanceof Hero)
+						.forEach(x -> ((Movable) x).setVx(-10));
+				} else if (code == KeyCode.UP) {
+					GameMap.getRenderObjects().parallelStream()
+						.filter(x -> x instanceof Hero)
+						.forEach(x -> ((Movable) x).setVy(-20));
+				} else if (code == KeyCode.RIGHT) {
+					GameMap.getRenderObjects().parallelStream()
+						.filter(x -> x instanceof Hero)
+						.forEach(x -> ((Movable) x).setVx(10));
+				}
+			}
+		});
+		
+		gameCanvas.setOnKeyReleased(event -> {
+			KeyCode code = event.getCode();
+			if (code.isArrowKey()) {
+				if (code == KeyCode.LEFT || code == KeyCode.RIGHT) {
+					GameMap.getRenderObjects().parallelStream()
+						.filter(x -> x instanceof Hero)
+						.forEach(x -> ((Movable) x).setVx(0));
+				}
+			}
+		});
+	}
+	
+}
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class MainController {
+import view.GamePane;
+import view.ViewManager;
 
+public class MainController {
+	
+	ViewManager viewManager;
+	GameCanvasController gameCanvasController;
+	
+	public MainController() {
+		viewManager = ViewManager.getInstance();
+		gameCanvasController = new GameCanvasController(((GamePane) viewManager.getPane("game")).getGameCanvas());
+	}
+	
+	public void on() {
+		gameCanvasController.listenEvent();
+	}
 }
