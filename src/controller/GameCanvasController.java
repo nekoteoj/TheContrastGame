3b35ca344dbb5a47d7f06f18e911c770d4eab214
Pisit Wajanasara <compisit1234@gmail.com>
2017-12-05 10:54:31 +0700

    Add key polling

--- src/controller/GameCanvasController.java
+++ src/controller/GameCanvasController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
 import javafx.scene.input.KeyCode;
 import logic.GameMap;
 import model.Bullet;
@@ -9,56 +12,60 @@ import model.NormalBullet;
 import view.GameCanvas;
 
 public class GameCanvasController {
-	
+
 	GameCanvas gameCanvas;
-	
+
+	private Set<KeyCode> pressedKey;
+
 	public GameCanvasController(GameCanvas gameCanvas) {
 		this.gameCanvas = gameCanvas;
+		this.pressedKey = new ConcurrentSkipListSet<>();
 	}
-	
+
 	public void listenEvent() {
+//		Hero hero = (Hero) GameMap.getRenderObjects()
+//			.parallelStream()
+//			.filter(x -> x instanceof Hero)
+//			.findAny()
+//			.get();
 		gameCanvas.setOnKeyPressed(event -> {
 			KeyCode code = event.getCode();
-			if (code.isArrowKey()) {
+			if (!pressedKey.contains(code)) {
 				if (code == KeyCode.LEFT) {
-					GameMap.getRenderObjects().parallelStream()
-						.filter(x -> x instanceof Hero)
-						.forEach(x -> ((Movable) x).setVx(-10));
-				} else if (code == KeyCode.UP) {
-					GameMap.getRenderObjects().parallelStream()
-						.filter(x -> x instanceof Hero)
-						.forEach(x -> {
-							((Movable) x).setVy(-20);
-							((Hero) x).setOnAir(true);
-						});
-				} else if (code == KeyCode.RIGHT) {
-					GameMap.getRenderObjects().parallelStream()
-						.filter(x -> x instanceof Hero)
-						.forEach(x -> ((Movable) x).setVx(10));
+					GameMap.getRenderObjects().parallelStream().filter(x -> x instanceof Hero)
+							.forEach(x -> ((Movable) x).setVx(-10));
 				}
-			} else if (code.isWhitespaceKey()) {
-				 if (code == KeyCode.SPACE) {
-						Hero hero = GameMap.getRenderObjects().parallelStream()
-								.filter(x -> x instanceof Hero)
-								.map(x -> (Hero) x)
-								.findAny()
-								.get();
-						Bullet bullet = new NormalBullet(hero.getPosition().first + hero.getWidth(), hero.getPosition().second + hero.getHeight() / 2, 0, hero.getDirection());
-						GameMap.addEntity(bullet);
-					}
+				if (code == KeyCode.UP) {
+					GameMap.getRenderObjects().parallelStream().filter(x -> x instanceof Hero).forEach(x -> {
+						((Movable) x).setVy(-20);
+						((Hero) x).setOnAir(true);
+					});
+				}
+				if (code == KeyCode.RIGHT) {
+					GameMap.getRenderObjects().parallelStream().filter(x -> x instanceof Hero)
+							.forEach(x -> ((Movable) x).setVx(10));
+				}
+				if (code == KeyCode.Z) {
+					Hero hero = GameMap.getRenderObjects().parallelStream().filter(x -> x instanceof Hero).map(x -> (Hero) x)
+							.findAny().get();
+					Bullet bullet = new NormalBullet(hero.getPosition().first + hero.getWidth(),
+							hero.getPosition().second + hero.getHeight() / 2, 0, hero.getDirection());
+					GameMap.addEntity(bullet);
+				}
+				pressedKey.add(code);
 			}
 		});
-		
+
 		gameCanvas.setOnKeyReleased(event -> {
 			KeyCode code = event.getCode();
 			if (code.isArrowKey()) {
 				if (code == KeyCode.LEFT || code == KeyCode.RIGHT) {
-					GameMap.getRenderObjects().parallelStream()
-						.filter(x -> x instanceof Hero)
-						.forEach(x -> ((Movable) x).setVx(0));
+					GameMap.getRenderObjects().parallelStream().filter(x -> x instanceof Hero)
+							.forEach(x -> ((Movable) x).setVx(0));
 				}
 			}
+			pressedKey.remove(code);
 		});
 	}
-	
+
 }
