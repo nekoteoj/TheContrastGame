b1e0c86789995653eb504135b97ac2cb3a2f4402
Pisit Wajanasara <compisit1234@gmail.com>
2017-12-05 10:57:06 +0700

    Add Transition

--- src/view/GameCanvas.java
+++ src/view/GameCanvas.java
@@ -8,10 +8,13 @@ public class GameCanvas extends Canvas {
 	
 	private int startX = 0;
 	private static GameCanvas currentInstance = null;
+	private boolean onTransition;
+	private int transitionWidth = 150;
 	
 	public GameCanvas() {
 		super(App.SCREEN_WIDTH, App.SCREEN_HEIGHT);
 		currentInstance = this;
+		this.onTransition = false;
 	}
 	
 	public void setStartX(int startX) {
@@ -27,14 +30,27 @@ public class GameCanvas extends Canvas {
 	}
 	
 	public void checkStartPoint(Hero hero) {
-		if (hero.getPosition().first - startX > App.SCREEN_WIDTH - 150) {
-			startX = hero.getPosition().first - (App.SCREEN_WIDTH - 150);
+		if (onTransition) {
+			doTansition(hero);
+		}
+		if (hero.getPosition().first - startX > App.SCREEN_WIDTH - 150 && !onTransition) {
+			//startX = hero.getPosition().first - (App.SCREEN_WIDTH - 150);
+			onTransition = true;
 		}
 		if (hero.getPosition().first - startX < 150 - hero.getWidth()) {
 			startX = hero.getPosition().first - 150 + hero.getWidth();
 			if (startX < 0) {
 				startX = 0;
 			}
+		}
+	}
+	
+	public void doTansition(Hero hero) {
+		startX = hero.getPosition().first - (App.SCREEN_WIDTH - transitionWidth);
+		transitionWidth += 15;
+		if (transitionWidth >= 2 * App.SCREEN_WIDTH / 3 + hero.getWidth()) {
+			onTransition = false;
+			transitionWidth = 150;
 		}
 	}
 
