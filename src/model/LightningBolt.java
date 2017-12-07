69d3449615affe7bcd07c6c4064ee4a49bf27cbc
Pawin Piemthai <pawin35@hotmail.com>
2017-12-07 22:47:11 +0700

    add sound to a lightning

--- src/model/LightningBolt.java
+++ src/model/LightningBolt.java
@@ -1,13 +1,20 @@
 package model;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.media.AudioClip;
 import logic.GameMap;
+import model.utility.ClassResourceUtility;
 
 public class LightningBolt implements Renderable{
+
+	private static AudioClip lightningSound;
+	static {
+	lightningSound = new AudioClip(ClassResourceUtility.getResourcePath("sound/lightning.wav"));
+	}
 	
 	public LightningBolt(int damage) {
 	GameMap.getEntityObjects().parallelStream().filter(x -> x instanceof Hero).forEach(e -> ((Hero)e).decreaseHp(damage));	
-	
+lightningSound.play();	
 	}
 
 	@Override
