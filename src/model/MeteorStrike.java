7bc35de18b4d38c24ae293d235cc7ff42959747a
Pawin Piemthai <pawin35@hotmail.com>
2017-12-07 22:33:33 +0700

    Custom collide for Meteor

--- src/model/MeteorStrike.java
+++ src/model/MeteorStrike.java
@@ -8,6 +8,7 @@ import javafx.scene.image.Image;
 import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
 import logic.GameMap;
+import model.map.MapObject;
 import model.utility.ClassResourceUtility;
 import model.utility.Pair;
 import view.GameCanvas;
@@ -23,15 +24,16 @@ public class MeteorStrike extends Bullet {
 		fallingSound = new AudioClip(ClassResourceUtility.getResourcePath("sound/falling_meteor.wav"));
 		impactSound = new AudioClip(ClassResourceUtility.getResourcePath("sound/impact_meteor.wav"));
 	}
-private int meteorWidth = 100;
-private int meteorHeight = 100;
-private int centerX;
+protected int meteorWidth = 100;
+protected int meteorHeight = 100;
+protected int centerX;
 	
 public MeteorStrike(int x, int y, int target) {
 	super(0, 0, 1,1);
 int meteorX = x - ((this.meteorWidth - 50)/2);
 meteorX = meteorX < 0? 0: meteorX;
-this.centerX = (meteorX + this.width)/2;
+this.centerX = (meteorX + (meteorX +this.meteorWidth))/2;
+//System.out.println("original center:"+this.centerX); 
 this.width = this.meteorWidth;
 this.height = this.meteorHeight;
 		this.position.first = meteorX;
@@ -48,10 +50,29 @@ gc.setFill(Color.RED);
 		gc.fillOval(position.first - GameCanvas.getCurrentInstance().getStartX(), position.second, width, height);
 }
 
+	@Override
+	public void checkCollide() {
+		for (Entity e : GameMap.getEntityObjects()) {
+			if (this != e && e.isCollide(this)) {
+				if (target == 0 && e instanceof Enemy) {
+					fixCollide(e);
+					dead();
+					break;
+				} else if (target == 1 && e instanceof Hero) {
+					fixCollide(e);
+					dead();
+					break;
+				} else if (e instanceof MapObject){
+					dead();
+					break;
+				}
+				}
+			}
+		}
 
 @Override
 public void dead() {
-//	System.out.println("Meteor: (" + this.position.first + ", " + this.position.second + ")");
+	System.out.println("Meteor: (" + this.position.first + ", " + this.position.second + ")");
 impactSound.play();
 		GameMap.getEntityObjects().remove(this);
 		GameMap.getRenderObjects().remove(this);
